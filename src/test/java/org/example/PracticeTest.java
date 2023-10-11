a3f2d41460476e1b8e09449be8823e0ca905bb18
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2023-10-10 22:57:44 -0500

    Revert back to JUnit 4

--- src/test/java/org/example/PracticeTest.java
+++ src/test/java/org/example/PracticeTest.java
@@ -1,13 +1,9 @@
 package org.example;
 
 
+import org.junit.Test;
 
-
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.Assert.assertEquals;
 
 public class PracticeTest {
 
